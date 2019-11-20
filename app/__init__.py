caff7036ec88c5794ddf9dcbb872dbeb9b377a5b
daniel_ubuntu_house <jdanielcarranzal@hotmail.com>
2019-11-20 08:10:06 -0500

    deleted unused lines and commeds were added

--- app/__init__.py
+++ app/__init__.py
@@ -4,8 +4,8 @@ from config import config
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
-    config[config_name].init_app(app)
     
+    # a blueprint is used to register functionalities accross different files
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
